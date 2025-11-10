aea7240f2bbc6dc9e0fa0d81047eb9cf733d1f0f
Petro <breazu1515@gmail.com>
2025-11-10 19:01:18 +0100

    Cambio de tema

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World and Petro!");
+        System.out.println("Hello, Albertito and Petro!");
     }
 }
