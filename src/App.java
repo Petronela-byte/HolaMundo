b4803c0937019fb76a1a44a0a5e4f748c713748e
Petro <breazu1515@gmail.com>
2025-11-15 16:49:53 +0100

    invasor

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Albertito and Petro!");
+        System.out.println("Hello, invasor!");
     }
 }
